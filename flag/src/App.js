04e9a5fd2f1c11343c5e542639459fc3e03f013a
vikasthakurr <vikaskumar20012001@gmail.com>
2023-01-27 10:04:04 +0530

    first commit by thakur

---
+++ flag/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import India from './Component/India';
+
+const App = () => {
+  return (
+    <>
+      <India />
+    </>
+  )
+}
+
+export default App;
---
+++ flag/src/Component/India.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './India.css';
+
+const India = () => {
+   return (
+      <>
+
+         <div className="wrapper">
+            <div className="upper"></div>
+            <div className="middle">
+               <div class="wheel">
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+                  <span class="line"></span>
+               </div>
+            </div>
+            <div className="lower"></div>
+         </div>
+      </>
+
+   )
+}
+
+export default India;
